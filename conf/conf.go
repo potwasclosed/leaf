3800d173c6dbdb4a107fd3b65537d46e65d93282
potwasclosed <1683721999@qq.com>
2022-01-06 22:55:27 +0800

    Update conf.go

--- conf/conf.go
+++ conf/conf.go
@@ -15,6 +15,6 @@ var (
 
 	// cluster
 	ListenAddr      string
-	ConnAddrs       []string
+	ConnAddrs       []string  //A,B两个进程 分别监听不同的端口,相互连通  是两个单独的client,单独的连接. 感觉作者像这个意思.
 	PendingWriteNum int
 )
